240de0aa4f49ac517f14736d8284ae0e8d1aa58a
ocgordon <ocgordon@github.com>
2020-02-12 11:24:07 -0500

    basic timer

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eyextension
+
 a chrome extension to remind you to look at something else for 20 seconds every 20 minutes
+
+# status
+
+¯\\\_(ツ)\_/¯
---
+++ content.js
@@ -0,0 +1,29 @@
+function startTimer(duration, display) {
+  var start = Date.now(),
+    diff,
+    minutes,
+    seconds;
+  function timer() {
+    diff = duration - (((Date.now() - start) / 1000) | 0);
+
+    minutes = (diff / 60) | 0;
+    seconds = diff % 60 | 0;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    display.textContent = minutes + ':' + seconds;
+
+    if (diff <= 0) {
+      start = Date.now() + 1000;
+    }
+  }
+  timer();
+  setInterval(timer, 1000);
+}
+
+window.onload = function() {
+  var twentyMinutes = 60 * 20,
+    display = document.querySelector('#time');
+  startTimer(twentyMinutes, display);
+};
